2e337b93f52d268c94e556797d4bcd2bd1f27be7
Andrew-Ekrame <andrewekrame2@gmail.com>
2024-09-13 18:25:39 +0300

    Refactored game logic, Made the README file

---
+++ README.md
@@ -0,0 +1,72 @@
+# Guess My Number
+
+![Guess My Number 1](game_images/guessMyNumber1.png)
+![Guess My Number 2](game_images/guessMyNumber2.png)
+![Guess My Number 3](game_images/guessMyNumber3.png)
+![Guess My Number 4](game_images/guessMyNumber4.png)
+![Guess My Number 5](game_images/guessMyNumber5.png)
+
+This is a simple number guessing game developed as part of Jonas Schmedtmann's JavaScript course. The player has to guess a number between 1 and 20, and the game provides feedback on whether the guess is too high, too low, or correct.
+
+## Table of Contents
+
+- [Project Overview](#project-overview)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Usage](#usage)
+
+## Project Overview
+
+"Guess My Number" is a fun and interactive guessing game where the user tries to guess a secret number between 1 and 20. The game provides feedback on the guesses and keeps track of the user's score and high score.
+
+## Features
+
+- Randomly generated secret number between 1 and 20.
+- User input to guess the number.
+- Feedback on whether the guess is too high, too low, or correct.
+- Score tracking for the current game.
+- High score tracking across games.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Usage
+
+1. **Open the game in your web browser:**
+
+   - After setting up the project, navigate to the project directory and open `index.html` in your preferred web browser.
+
+2. **Enter a guess:**
+
+   - You will see an input field where you can enter a number between 1 and 20.
+
+3. **Submit your guess:**
+
+   - Click the "Check!" button to submit your guess.
+
+4. **Receive feedback:**
+
+   - The game will provide feedback on whether your guess is too high, too low, or correct.
+     - If your guess is too high, you will see a message indicating it is too high.
+     - If your guess is too low, you will see a message indicating it is too low.
+     - If your guess is correct, you will see a congratulatory message and the background color of the game will change to indicate you have won.
+
+5. **Track your score:**
+
+   - The game keeps track of your score, which decreases by one point for each incorrect guess.
+   - The current score is displayed on the screen.
+
+6. **High score tracking:**
+
+   - The game also keeps track of the highest score achieved, which is updated whenever you set a new high score.
+
+7. **Start a new game:**
+
+   - Click the "Again!" button to reset the game and start a new round with a new secret number.
+   - The score and the input field will be reset, but the high score will be retained.
+
+8. **Play again:**
+   - Repeat the steps to keep playing and try to beat your high score.
--- script.js
+++ script.js
@@ -9,48 +9,60 @@ let guess = document.querySelector(".guess");
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let number = document.querySelector(".number");
 let body = document.querySelector("body");
+const displayMessage = function (messageText) {
+  message.textContent = messageText;
+};
+const scoreValueController = function (value) {
+  score.textContent = value;
+  scoreValue = value;
+};
+const secretNumberGenerator = function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+};
+const styleChange = function () {
+  number.style.width = "30rem";
+  body.style.backgroundColor = "#60b347";
+};
+const styleReset = function () {
+  number.style.width = "15rem";
+  body.style.backgroundColor = "#222";
+};
+const textContentReset = function () {
+  guess.value = "";
+  number.textContent = "?";
+  score.textContent = scoreValue;
+};
+const checkHighScore = function () {
+  if (highScoreValue < scoreValue) {
+    highScore.textContent = scoreValue;
+    highScoreValue = scoreValue;
+  }
+};
 document.querySelector(".check").addEventListener("click", function () {
   let guessValue = Number(guess.value);
   if (!guessValue) {
-    message.textContent = "⛔ No number!";
+    displayMessage("⛔ No number!");
   } else if (guessValue === secretNumber) {
-    message.textContent = "🎉 Correct Number!";
+    displayMessage("🎉 Correct Number!");
     number.textContent = secretNumber;
-    number.style.width = "30rem";
-    body.style.backgroundColor = "#60b347";
-    if (highScoreValue < scoreValue) {
-        highScore.textContent = scoreValue;
-        highScoreValue = scoreValue;
-    }
-  } else if (guessValue > secretNumber) {
+    styleChange();
+    checkHighScore();
+  } else {
     if (scoreValue > 1) {
-      message.textContent = "📈 Too high!";
-      scoreValue--;
-      score.textContent = scoreValue;
+      displayMessage(
+        guessValue > secretNumber ? "📈 Too high!" : "📉 Too low!"
+      );
+      scoreValueController(scoreValue - 1);
     } else {
-      message.textContent = "💥 You lost the game!";
-      scoreValue = 0;
-      score.textContent = scoreValue;
-    }
-  } else if (guessValue < secretNumber) {
-    if (scoreValue > 1) {
-      message.textContent = "📉 Too low!";
-      scoreValue--;
-      score.textContent = scoreValue;
-    } else {
-      message.textContent = "💥 You lost the game!";
-      scoreValue = 0;
-      score.textContent = scoreValue;
+      displayMessage("💥 You lost the game!");
+      scoreValueController(0);
     }
   }
 });
 document.querySelector(".again").addEventListener("click", function () {
   scoreValue = 20;
-  score.textContent = scoreValue;
-  message.textContent = "Start guessing...";
-  number.textContent = "?";
-  number.style.width = "15rem";
-  body.style.backgroundColor = "#222";
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  guess.value = "";
+  displayMessage("Start guessing...");
+  styleReset();
+  secretNumberGenerator();
+  textContentReset();
 });

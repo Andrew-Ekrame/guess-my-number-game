58a48daa267ce93ac5df849aded8db3aca9cd656
Andrew-Ekrame <andrewekrame2@gmail.com>
2024-09-13 16:44:07 +0300

    Added event listener, check for empty value

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+let message = document.querySelector(".message");
+let score = document.querySelector(".score");
+let guess = document.querySelector(".guess");
+document.querySelector(".check").addEventListener("click", function () {
+  let guessValue = guess.value;
+  if (!guessValue) {
+    message.textContent = "⛔ No number!";
+  }
+});
